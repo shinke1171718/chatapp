06f334a2e6f649e7a02099c339c65819c3b79fee
Apple <apple@ShinkenoMacBook.local>
2023-03-10 20:25:30 +0900

    convesation#showにpathを通すことができた

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -2,14 +2,6 @@ class ConversationsController < ApplicationController
 
   def index
     @user = User.all
-    @users = User.all
-  end
-
-  def create
-    current_users_chat_rooms = Conversation.where(user_id: current_user.id)
-    @messages = Message.all
-    @conversation = current_user.Conversation.new
-    
   end
 
   def show
